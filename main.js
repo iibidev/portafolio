1ac74ee03b380319468f57e75806d514dc560eba
iibidev <ibai.amo.ballesteros@gmail.com>
2025-10-09 19:33:45 +0200

    welcome animation added

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ const html = document.documentElement;
 const changeModeBtn = $("#changeModeBtn");
 const modeIcon = $(".lightDarkModeBtn > i");
 
+const welcome = $(".welcome");
+const welcomeAnimation = $(".welcome__animation");
+const welcomePic = $(".welcome__pic");
+const welcomeBtn = $(".welcome__enter");
+const backgroundMask = $(".backgroundMask");
+
 changeModeBtn.addEventListener("click", ()=>{
     const mode = html.getAttribute("data-theme");
     if(mode === "dark"){
@@ -14,4 +20,41 @@ changeModeBtn.addEventListener("click", ()=>{
         html.setAttribute("data-theme", "dark");
         modeIcon.classList.replace("ri-moon-line", "ri-sun-line");
     }
+});
+
+welcomeBtn.addEventListener("click", ()=>{
+    const anim = welcomeAnimation.animate(
+        [
+            { transform: 'translate(-50%, 80px)', opacity: 1 },
+            { transform: 'translate(-50%, 200px)', opacity: 0 },
+        ],
+        {
+            duration: 800,
+            easing: 'ease',
+            fill: "forwards"
+        }
+    );
+    
+    setTimeout(()=>{
+        const anim2 = welcomePic.animate(
+            [
+                { width: '150px', opacity: 1 },
+                { opacity: 0 },
+                { width: '1000vw', opacity: 0 }
+            ],
+            {
+                duration: 6000,
+                fill: "forwards",
+                easing: "ease",
+            }
+        )
+
+        backgroundMask.classList.add("masked");
+        setTimeout(()=>{
+            backgroundMask.style.height = "auto";
+            backgroundMask.style.overflow = "auto"; 
+            backgroundMask.style.maskImage = "none"; 
+            welcome.style.display = "none";           
+        }, 2100);
+    }, 300);
 });
